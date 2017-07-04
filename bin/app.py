e550e842920e2ee88490a6f8d5ec7435e42ba283
Jonathan Yantz <ijryantz@gmail.com>
2017-07-03 22:44:43 -0400

    changed port call

--- bin/app.py
+++ bin/app.py
@@ -1,3 +1,4 @@
+import os
 import web
 
 urls = (
@@ -14,6 +15,6 @@ class Index(object):
         return render.index(greeting = greeting)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
-    # app.run()
+    # port = int(os.environ.get('PORT', 8080))
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
